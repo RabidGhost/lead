def4b8ff579b7c574b4703a6bd74d9b16662e2bb
Mitchell Browne <mitchellbrowne01@gmail.com>
2024-06-18 17:21:53 +1000

    improve webapi error handling

--- lead-w/src/lib.rs
+++ lead-w/src/lib.rs
@@ -6,10 +6,13 @@ use wasm_bindgen::prelude::*;
 pub fn build_from_string(string: String) -> String {
     let instructions: Vec<Instruction> = Pipeline::Text(string, None)
         .lex()
+        .map_err(|err| return err.to_string())
         .unwrap()
         .parse()
+        .map_err(|err| return err.to_string())
         .unwrap()
         .build()
+        .map_err(|err| return err.to_string())
         .unwrap()
         .into();
 
