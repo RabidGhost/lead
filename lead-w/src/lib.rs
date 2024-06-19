7c5c54df04298b1ea7df51c83edd86323de5b7f9
Mitchell Browne <mitchellbrowne01@gmail.com>
2024-06-19 18:04:32 +1000

    add span computing to leadw

--- lead-w/src/lib.rs
+++ lead-w/src/lib.rs
@@ -1,6 +1,19 @@
+use lead::air::Inst;
+use lead::lex::token::Token;
 use lead_vm::air::Instruction;
 use leadc::pipeline::Pipeline;
+use serde::Serialize;
+use sourcespan::SourceSpan;
 use wasm_bindgen::prelude::*;
+
+mod sourcespan;
+
+#[derive(Serialize)]
+pub struct Message {
+    ed_lang_spans: Vec<SourceSpan>,
+    air_text: String,
+    air_spans: Vec<SourceSpan>,
+}
 
 #[wasm_bindgen]
 pub fn build_from_string(string: String) -> String {
@@ -10,7 +23,7 @@ pub fn build_from_string(string: String) -> String {
             Err(err) => return err.to_string(),
             Ok(pipeline) => match pipeline.build() {
                 Err(err) => return err.to_string(),
-                Ok(pipeline) => pipeline.into(),
+                Ok(pipeline) => pipeline.try_into().unwrap(),
             },
         },
     };
@@ -21,4 +34,41 @@ pub fn build_from_string(string: String) -> String {
         buf.push_str(&format!("{instruction}"));
     }
     buf
+}
+
+#[wasm_bindgen]
+pub fn build_and_generate_spans(string: String) -> String {
+    let lexed_pipeline = match Pipeline::Text(string, None).lex() {
+        Err(err) => return err.to_string(),
+        Ok(pipeline) => pipeline,
+    };
+
+    let tokens: Vec<Token> = lexed_pipeline.clone().into();
+
+    let ed_lang_spans: Vec<SourceSpan> = tokens.iter().map(|tok| tok.span().into()).collect();
+
+    let instructions: Vec<Inst> = match lexed_pipeline.parse() {
+        Err(err) => return err.to_string(),
+        Ok(pipeline) => match pipeline.build() {
+            Err(err) => return err.to_string(),
+            Ok(pipeline) => pipeline.try_into().unwrap(),
+        },
+    };
+
+    let (air_text, air_spans) = sourcespan::render_and_compute_spans(instructions);
+
+    let message = Message {
+        ed_lang_spans,
+        air_text,
+        air_spans,
+    };
+
+    let serialized = serde_json::to_string(&message).unwrap();
+    serialized
+    // let mut buf: String = String::new();
+
+    // for instruction in instructions {
+    //     buf.push_str(&format!("{instruction}"));
+    // }
+    // buf
 }
---
+++ lead-w/src/sourcespan.rs
@@ -0,0 +1,41 @@
+use lead::{air::Inst, lex::span::*};
+use primes::factors_uniq;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct SourceSpan {
+    start: usize,
+    end: usize,
+    ids: Vec<u64>,
+}
+
+impl Into<SourceSpan> for Span {
+    fn into(self) -> SourceSpan {
+        SourceSpan {
+            start: self.span().0,
+            end: self.span().1,
+            ids: factors_uniq(self.id()),
+        }
+    }
+}
+
+pub fn render_and_compute_spans(instructions: Vec<Inst>) -> (String, Vec<SourceSpan>) {
+    let mut rendered_instructions: String = String::new();
+    let mut spans: Vec<SourceSpan> = Vec::new();
+
+    for inst in instructions {
+        let Inst { instruction, span } = inst;
+        let start = rendered_instructions.len();
+        rendered_instructions.push_str(&format!("{instruction}\n"));
+        let end = rendered_instructions.len();
+        // this might need to be len - 1
+        //
+
+        spans.push(SourceSpan {
+            start,
+            end,
+            ids: factors_uniq(span.id()),
+        });
+    }
+    (rendered_instructions, spans)
+}
