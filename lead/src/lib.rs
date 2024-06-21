db409091056d8a2f5fa7657ea1b531a844f00021
Mitchell Browne <mitchellbrowne01@gmail.com>
2024-06-21 10:32:05 +1000

    add trait for transcoding

--- lead/src/lib.rs
+++ lead/src/lib.rs
@@ -2,3 +2,4 @@ pub mod air;
 pub mod error;
 pub mod lex;
 pub mod parse;
+pub mod transcode;
---
+++ lead/src/transcode.rs
@@ -0,0 +1,11 @@
+use crate::air::Inst;
+use crate::lex::span::Span;
+
+pub struct Line {
+    text: String,
+    span: Span,
+}
+
+pub trait Transcoder {
+    fn transcode(instructions: Vec<Inst>) -> Vec<Line>;
+}
